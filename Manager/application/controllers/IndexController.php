23496317dbeefc46572e770b2803b4456f46d709
Vijay Lathiya <lathiyav2810@gmail.com>
2021-05-10 16:27:22 +0530

    insert value

--- Manager/application/controllers/IndexController.php
+++ Manager/application/controllers/IndexController.php
@@ -1,61 +1,35 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
-	public function indexAction() {		
-		$db = Zend_Db_Table::getDefaultAdapter();
-		// $this->view->nofooter;
+	class IndexController extends Zend_Controller_Action {
+		public function indexAction() {		
+			$db = Zend_Db_Table::getDefaultAdapter();
+			// $this->view->nofooter;
 
-		$image = "";
-		$name = "";
-		$slug = "";
-		$parent_category = "";
-		$group_category = "";
-		$filterable_attributes = "";
-		$keywords = "";
-		$featured_brands = "";
-		$meta_image = "";
-		$meta_title = "";
-		$meta_description = "";
-		$meta_keywords = "";
+			if (isset($_POST['submit'])) {
 
-		if (isset($_POST['submit'])) {
+				if ($this->getRequest()->isPost()) {
+					$data = $this->_request->getPost();
 
-			if ($this->getRequest()->isPost()) {
-
-				$image = $this->_request->getPost('image');
-				$name = $this->_request->getPost('name');
-				$slug = $this->_request->getPost('slug');
-				$parent_category = $this->_request->getPost('parent_category');
-				$group_category = $this->_request->getPost('group_category');
-				$filterable_attributes = $this->_request->getPost('filterable_attributes');
-				$keywords = $this->_request->getPost('keywords');
-				$featured_brands = $this->_request->getPost('featured_brands');
-				$meta_image = $this->_request->getPost('meta_image');
-				$meta_title = $this->_request->getPost('meta_title');
-				$meta_description = $this->_request->getPost('meta_description');
-				$meta_keywords = $this->_request->getPost('meta_keywords');
-
-				$_sets = array();
-				$_sets['image'] = $image;
-				$_sets['name'] = $name;
-				$_sets['slug'] = $slug;
-				$_sets['parent_category'] = $parent_category;
-				$_sets['group_category'] = $group_category;
-				$_sets['filterable_attributes'] = $filterable_attributes;
-				$_sets['keywords'] = $keywords;
-				$_sets['featured_brands'] = $featured_brands;
-				$_sets['meta_image'] = $meta_image;
-				$_sets['meta_title'] = $meta_title;
-				$_sets['meta_description'] = $meta_description;
-				$_sets['meta_keywords'] = $meta_keywords;
-				// die();
-				echo json_encode($_sets);
-
-				// die();
-				$this->db->insert("arcedior-list", $_sets);
+					$_sets = array();
+					$_sets['image'] = $data['image'];
+					$_sets['name'] = $data['name'];
+					$_sets['slug'] = $data['slug'];
+					$_sets['parent_category'] = $data['parent_category'];
+					$_sets['group_category'] = $data['group_category'];
+					$_sets['filterable_attributes'] = $data['filterable_attributes'];
+					$_sets['keywords'] = $data['keywords'];
+					$_sets['featured_brands'] = $data['featured_brands'];
+					$_sets['meta_image'] = $data['meta_image'];
+					$_sets['meta_title'] = $data['meta_title'];
+					$_sets['meta_description'] = $data['meta_description'];
+					$_sets['meta_keywords'] = $data['meta_keywords'];
+		
+					$this->db->insert("arcedior-list", $_sets);
+				}
 			}
 		}
+		
+
+
 	}
-}
 ?>
